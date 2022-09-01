e09012192b7cc8f2189eb8538ec0894afbffcae3
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-09-01 14:30:49 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ abstract class Show {
   ) {}
 
   abstract getDuration(): number;
+
+  getName(): string {
+    return this.name;
+  }
 }
 
 class Movie extends Show {
@@ -21,6 +25,7 @@ class Movie extends Show {
     return this.duration;
   }
 }
+
 class Episode extends Show {
   constructor(
     protected name: string,
@@ -44,7 +49,6 @@ class Series extends Show {
     protected episodes: Episode[]
   ) {
     super(name, genre, releaseDate);
-    this.episodes = episodes;
   }
 
   getDuration() {
@@ -53,6 +57,91 @@ class Series extends Show {
     }, 0);
   }
 }
+
+class StreamingService {
+  private shows: Show[] = [];
+  protected viewsByShowNames: Map<Show['name'], number> = new Map();
+  //change string of name to full object of film
+
+  constructor(protected name: string) {}
+
+  addShow(show: Show): void {
+    if (this.shows.includes(show)) {
+      console.log(`${show.getName()} is already in library`);
+      return;
+    }
+    this.shows.push(show);
+  }
+
+  getShows(): Show[] {
+    return this.shows;
+  }
+
+  addViewByShowNames(showName: string): void {
+    let views = this.viewsByShowNames.get(showName);
+    views ? (views += 1) : (views = 1);
+
+    this.viewsByShowNames.set(showName, views);
+  }
+
+  getMostViewedShowsOfYear(year: number): [] {
+    //find all films from that yeart
+    //release date
+    //sort them from most viewed to less (values)
+    console.log(year);
+    this.viewsByShowNames.forEach((value, key) => {
+      console.log(key, value);
+    });
+
+    //     повертає до десяти найбільш
+    // переглянутих шоу, які вийшли в заданому році (менше десяти, якщо загальна
+    // кількість шоу менша за 10).
+    return [];
+  }
+  //   getMostViewedShowsOfGenre(genre): void {
+
+  //    //
+  //   }
+}
+
+class Subscription {
+  constructor(protected streamingService: StreamingService) {}
+
+  watch(showName: string): void {
+    const isShowsListEmpty = !(this.streamingService.getShows().length > 0);
+    const isQueriedShowAddedToService = !this.streamingService
+      .getShows()
+      .find(show => show.getName() === showName);
+
+    if (isShowsListEmpty) return;
+    if (isQueriedShowAddedToService) {
+      console.log(`${showName} is not available in StreamingService`);
+      return;
+    }
+
+    console.log(`Watching ${showName}`);
+    this.streamingService.addViewByShowNames(showName);
+  }
+
+  // getRecommendationTrending(): void {
+  //   this.streamingService.getMostViewedShowsOfYear();
+  //   //рандомно вибирає елемент масиву, сортує по тривалості.
+  // }
+  // getRecommendationByGenre(genre: string): void {
+  //   this.streamingService.getMostViewedShowsOfGenre();
+  //   //рандомно вибирає елемент масиву, сортує по тривалості.
+  // }
+}
+
+// class User {
+//   private subscriptions: [];
+//   constructor() {}
+
+//   subscribe(streamingService: {}): {} {
+//     this.subscriptions.push(streamingService);
+//     return this.subscriptions;
+//   }
+// }
 
 ///////////Execution
 const movie = new Movie(
@@ -82,60 +171,25 @@ const series = new Series(
   [episode1, episode2]
 );
 
-console.log(movie);
-console.log(episode1);
-console.log(series);
-console.log(series.getDuration());
+// console.log(movie);
+// console.log(episode1);
+// console.log(series);
+// console.log(series.getDuration());
+/////////////////////////////////////////////
 
-// class StreamingService(){
-//   private name: string;
-//   private shows: name: [];
-//   //viewsByShowNames:
+const netflix = new StreamingService('Netflix');
+console.log(netflix);
+netflix.addShow(movie);
+netflix.addShow(movie);
+netflix.addShow(series);
+console.log(netflix);
 
-//   addShow(show:string): void {
-//     shows.push(show);
-//   }
+const subscription = new Subscription(netflix);
+console.log('subscription', subscription);
+subscription.watch('Money Heist');
+subscription.watch('Interstellar');
+subscription.watch('Interstellar');
+subscription.watch('Spiderman');
+console.log('netflix', netflix);
 
-//   getMostViewedShowsOfYear(year): [] {
-//     //     повертає до десяти найбільш
-//     // переглянутих шоу, які вийшли в заданому році (менше десяти, якщо загальна
-//     // кількість шоу менша за 10).
-//     return []
-//   }
-
-//     getMostViewedShowsOfGenre(genre): void {
-//      //
-//     }
-
-// //     повертає до десяти найбільш
-// // переглянутих шоу, які належать до заданого жарну(менше десяти, якщо загальна
-// // кількість шоу менша за 10).
-
-// }
-
-// class Subscription {
-//   private streamingService: {};
-
-//   watch(showName: string): void {
-//     console.log(`Watching ${showName}`);
-//   }
-
-//   getRecommendationTrending(): void {
-//     this.streamingService.getMostViewedShowsOfYear();
-//     //рандомно вибирає елемент масиву, сортує по тривалості.
-//   }
-//   getRecommendationByGenre(genre: string): void {
-//     this.streamingService.getMostViewedShowsOfGenre();
-//     //рандомно вибирає елемент масиву, сортує по тривалості.
-//   }
-// }
-
-// class User {
-//   private subscriptions: [];
-//   constructor() {}
-
-//   subscribe(streamingService: {}): {} {
-//     this.subscriptions.push(streamingService);
-//     return this.subscriptions;
-//   }
-// }
+netflix.getMostViewedShowsOfYear(2017);
